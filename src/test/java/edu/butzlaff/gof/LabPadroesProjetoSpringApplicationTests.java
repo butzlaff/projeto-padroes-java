5337192361b79f8b47e64470bee90b3f132ae3c4
butzlaff <emiliobutz@gmail.com>
2023-10-21 17:53:42 -0300

    final project

---
+++ src/test/java/edu/butzlaff/gof/LabPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.butzlaff.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
